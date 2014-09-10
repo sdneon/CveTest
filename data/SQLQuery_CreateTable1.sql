dbb109b3662ffd277d9322f9a9b8f01f77bc7fad
sdneon <sdneon@hotmail.com>
2014-09-10 16:43:33 +0800

    Create SQLQuery_CreateTable1.sql

---
+++ data/SQLQuery_CreateTable1.sql
@@ -0,0 +1,21 @@
+USE [SQLDB]
+GO
+/****** Object:  Table [dbo].[Table1] ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Table1](
+	[AbcdefgID] [int] NOT NULL,
+	[Value1] [int] NULL,
+	[TypeID] [int] NULL,
+	[AbcdeValue] [int] NULL,
+	[AbcdeDesc] [varchar](50) NULL,
+	[AbcValue] [varchar](20) NULL,
+	[DefValue] [varchar](20) NULL
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
